b034dded1e99d85d774dc1c234c9ef9bf4043ed1
Stanley Wu <stanwu@umich.edu>
2023-07-15 11:39:04 -0700

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Message from './message';
+
+function App(){
+    return <div><Message /></div>; 
+}
+
+export default App;
---
+++ src/message.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//PascalCasing
+function Message(){
+    return <h1>hello world</h1>;
+}
+
+export default Message;
